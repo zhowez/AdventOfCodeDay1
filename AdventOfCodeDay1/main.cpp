8523c19f5da920ce7fadccc3ac62050fb3cd975b
James Z. Howes <zhowes14@icloud.com>
2020-12-05 19:00:39 -0500

    project complete

--- AdventOfCodeDay1/main.cpp
+++ AdventOfCodeDay1/main.cpp
@@ -6,9 +6,50 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+
+void addFileToArray(std::vector<int>&,std::ifstream&);
+
+int findAnswer(std::vector<int>&);
 
 int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
+	std::ifstream in;
+	in.open(argv[1]);
+	
+	if (!in.is_open()) {
+		std::cout << "The file could not open" << std::endl;
+		exit(-1);
+	}
+	
+	std::vector<int> arr;
+	addFileToArray(arr, in);
+	
+	std::cout << "The answer is " << findAnswer(arr) << "\n";
+	
+	
+
+	return 0;
+}
+
+
+void addFileToArray(std::vector<int> &arr,std::ifstream &inputFile) {
+	int num;
+	while (inputFile >> num) {
+		arr.push_back(num);
+	}
+	
+}
+
+int findAnswer(std::vector<int>& arr) {
+	
+	for (int i = 0; i <= arr.size(); ++i) {
+		for (int j = i + 1; j < arr.size(); ++j) {
+			if (arr[i] + arr[j] == 2020) {
+				return arr[i] * arr[j];
+			}
+		}
+	}
+	
 	return 0;
 }
